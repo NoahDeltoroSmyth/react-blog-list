9e587615448dd7ff3351f374d23ac5ac9fa3dcb6
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2021-12-06 15:12:38 -0800

    working app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/layout/Header/Header';
 import Footer from './components/layout/Footer/Footer';
+import { getBlogs } from '../src/services/blogs';
+import BlogCard from './components/BlogCard/BlogCard';
 
 function App() {
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    const grabData = async () => {
+      const data = await getBlogs();
+      setBlogs(data);
+    };
+    grabData();
+  }, []);
   return (
     <section className="main">
       <Header />
       <Footer />
+      {blogs.map((blog) => (
+        <BlogCard key={blog.id} {...blog} />
+      ))}
     </section>
   );
 }
